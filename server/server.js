826ba615b6ce9fcbaef0b4de2e6f9c135e0e3f1d
Rob Miller <robert.miller.nonemployee@pnc.com>
2017-09-28 14:04:07 -0400

    Adding websocket server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
+const expressWs = require('express-ws')(app);
+
 const transactions = require('./api/transactions-api');
 const clover = require('./api/clover-api');
+
+var sockets = [];
 
 // setup the app middlware
 require('./middleware/middleware')(app);
 
+app.ws('/connect', (ws, req) => {
+    ws.on('connect', () => {
+
+    });
+});
+
 // setup the apis
 app.use('/transactions/', transactions);
 app.use('/clover/', clover);
+
+setInterval(() => {
+    expressWs.getWss('/connect').clients.forEach(client => {
+        client.send('yo');
+    });
+}, 1000);
 
 // export the app for testing
 module.exports = app;
